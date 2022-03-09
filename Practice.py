81ac1366a1fd270efd9dd0d37fe4aef163ef9722
MQABBAS <101219974+MQABBAS@users.noreply.github.com>
2022-03-09 20:14:20 +0000

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,14 @@
+# My Code
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+bmi = int(weight) / float(height) ** 2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+
+if bmi > 24.9:
+    print('You are in the underweight range!')
+
+else:
+    print('You are in the overweight range!')
